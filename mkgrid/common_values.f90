fb55fb36bd8414a717cc5e48a1c3c710bf73797d
kim554 <agnesbischoff@gmail.com>
2023-07-05 17:09:15 -0400

    source file

---
+++ mkgrid/common_values.f90
@@ -0,0 +1,11 @@
+module common_values
+    
+  real, dimension(11) :: mass_points
+  real, dimension(3,11) :: hs_fid
+  real, dimension(4,11) :: ws_fid  
+  real, dimension(5) :: cy1, cy2, cy3, cy4, cy5, cfa, cfb, cfc, cfd
+  real, dimension(8) :: mass_points_for_h1
+  real, dimension(5) :: coeffs_menv, coeffs_mhe, coeffs_mh, coeffs_xhebar, coeffs_alph1, coeffs_alph2
+  real, dimension(5) :: coeffs_h2, coeffs_h3, coeffs_w1, coeffs_w2, coeffs_w3, coeffs_w4
+
+end module common_values
